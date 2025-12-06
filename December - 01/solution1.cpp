afd37daebff36669f2ed69de066ecfb8ad80bffd
deeksha9629 <deeksha9629@gmail.com>
2025-12-06 18:04:28 +0530

    solution1.cpp

--- December - 01/solution1.cpp
+++ December - 01/solution1.cpp
@@ -1,17 +1,20 @@
-
 #include <iostream>
 using namespace std;
 int main() {
     int n;
-    count=0;
-    cout<<"the number :"<<endl;
+    int count=0;
+    cout<<"The number :"<<endl;
     cin >> n;
     for(int i=1;i*i<=n;i++)
     {cout << i*i <<" " ;
     count++;
     }
-    cout<<"the count is:"<<count<<endl;
+    cout<<"\nthe count is:"<<count<<endl;
     
     return 0;
     
 }
+The number :
+90
+1 4 9 16 25 36 49 64 81 
+the count is:9
